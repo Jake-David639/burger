305c2f7de543bd8b194ae23ed8f72a6faba14c4f
Jake David <jakedgmu13@gmail.com>
2019-07-20 15:05:45 -0400

    completed burger.js model file

--- models/burger.js
+++ models/burger.js
@@ -1,6 +1,26 @@
-const orm = require('orm');
+const orm = require('../config/orm.js');
 
+const burger = {
 
-
+    all = (table, cb) => {
+        orm.selectAll().then((table) => {
+            console.log(table);
+        }).catch(err => {
+        console.log(err);
+        });
+    },
+    create = (name, cb) => {
+        orm.insertOne("burgers",["burger_name", "devoured"], 
+        [name, false], cb);
+    },
+    update = (id, cb) => {
+        let condition = `id=${id}`;
+        orm.updateOne("burgers", 
+        {
+            devoured: true
+        },
+        condition, cb);
+    }
+};
 
 module.exports = burger;
