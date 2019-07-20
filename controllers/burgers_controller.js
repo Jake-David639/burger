85f7487bb328199113bc5f304b5a9c28c2c2d459
Jake David <jakedgmu13@gmail.com>
2019-07-20 15:33:21 -0400

    most of the burger_controller.js file done

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,27 @@
+// import dependencies
+const express = require('express');
+const burger = require('../models/burger.js');
+const router = express.Router();
+
+// get route to render the homepage
+router.get('/', (req, res) => {
+    res.redirect('/burgers');
+});
+router.get('/burgers', (req, res) => {
+    burger.all((burgerData) => {
+        res.render("index", { burger_data: burgerData });
+    });
+});
+// post route to add new burger to db
+router.post('/burgers/create', (req, res) => {
+    burger.create(req.body.burger_name, result => {
+        console.log(result);
+        res.redirect('/');
+    });
+});
+// put route to update existing burger data
+router.put('/burgers/:id', (req, res) => {
+    
+});
+
+module.exports = router;
