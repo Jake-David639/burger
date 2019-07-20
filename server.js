bfe4fbe5ec093f9e9480a3f3d2a36c6035367ca9
Jake David <jakedgmu13@gmail.com>
2019-07-20 16:00:44 -0400

    added most of index.handlebars, WIP

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // dependencies
 const express = require('express');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers/burgers_controller.js');
 const PORT = 3306;
 
+// initiallizing the express app
 const app = express();
 // setting express to use the handlebars engine
 app.engine('handlebars', exphbs());
@@ -11,8 +13,9 @@ app.set('view engine', 'handlebars');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('index');
-})
-
+// setting express to serve static content from the public folder in the root directory
+app.use(express.static("public"));
+// setting express to use the routes from the burger controller file
+app.use(routes);
+// start listening on the indiated port
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
