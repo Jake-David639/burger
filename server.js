0f87b06c4446c712697891d0a3ad04c77c742464
Jake David <jakedgmu13@gmail.com>
2019-07-20 16:24:53 -0400

    added process.env.PORT option to the port in server.js for keroku support

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers/burgers_controller.js');
-const PORT = 3306;
+const PORT = process.env.PORT || 3306;
 
 // initiallizing the express app
 const app = express();
