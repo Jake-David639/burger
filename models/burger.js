ce2329dd11596182b02cbc1e91cd67a28469fc42
Jake David <jakedgmu13@gmail.com>
2019-07-20 16:15:22 -0400

    tweaked burger.js

--- models/burger.js
+++ models/burger.js
@@ -2,18 +2,18 @@ const orm = require('../config/orm.js');
 
 const burger = {
 
-    all = (table, cb) => {
+    all(table, cb) {
         orm.selectAll().then((table) => {
             console.log(table);
         }).catch(err => {
         console.log(err);
         });
     },
-    create = (name, cb) => {
+    create(name, cb) {
         orm.insertOne("burgers",["burger_name", "devoured"], 
         [name, false], cb);
     },
-    update = (id, cb) => {
+    update(id, cb) {
         let condition = `id=${id}`;
         orm.updateOne("burgers", 
         {
