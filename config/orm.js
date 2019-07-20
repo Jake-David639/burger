939597729153a54ee987320d79a3e6a58e85a985
Jake David <jakedgmu13@gmail.com>
2019-07-20 14:35:36 -0400

    added functions to the orm.js file

---
+++ config/orm.js
@@ -0,0 +1,46 @@
+// import the dependencies
+const connection = require('./connection');
+
+// create the ORM object to handle the MySQL queries
+const orm = {
+// todo
+    selectAll = (table, cb) => {
+       let queryStr = `SELECT * FROM ${tableInput};`;
+       connection.query(queryStr,(err, result) => {
+        if (err) throw err;
+        cb(result);
+    });
+},
+insertOne = (table, cols, vals, cb) => {
+    let queryStr = `INSERT INTO ${table} (${cols.join()}) VALUES (${questionMarkString(vals.length)})`;
+
+    console.log(queryStr);
+    
+    connection.query(queryStr, vals,(err, result) => {
+      if (err) throw err;
+      cb(result);
+    }); 
+},
+updateOne = () => {
+        
+    }
+}
+
+questionMarkString = number => {
+    let array = [];
+    for (let i = 0; i < number; i++) {
+        array.push("?");
+    }
+    return array.join();
+}
+
+convertObjectToSql = obj => {
+    let keyValArray = [];
+    Object.keys(obj).forEach(key => {
+        let value = obj[key];
+        if (typeof value === "string" && value.indexOf(" ") >= 0) value = `'${value}'`;
+        keyValArray.push(`${key}=${value}`);
+    });
+}
+
+module.exports = orm;
