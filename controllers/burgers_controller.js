980806564352b2f70ad45d13ad7f84584279ce09
Jake David <jakedgmu13@gmail.com>
2019-07-25 14:38:11 -0400

    tweaking routes file

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,9 +4,9 @@ const burger = require('../models/burger.js');
 const router = express.Router();
 
 // get route to render the homepage
-// router.get('/', (req, res) => {
-//     res.redirect('/burgers');
-// });
+router.get('/', (req, res) => {
+    res.redirect('/burgers');
+});
 router.get('/', (req, res) => {
     burger.all((burgerData) => {
         res.render("index", { burger_data: burgerData });
