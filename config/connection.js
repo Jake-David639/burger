c0b51081464224a0f2fe9d41d5525a0ec306f428
Jake David <jakedgmu13@gmail.com>
2019-07-25 14:14:27 -0400

    changed db connection to use heroku db uri

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,13 @@
 const mysql = require('mysql');
 
 // the config info the mysql db connection
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: process.env.PORT,
-    user: "root",
-    password: "",
-    database: "burgers_db"
-  });
+// heroku deployment db uri
+const connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 // create the connection, throw errors if encountered.
 connection.connect(err => {
     if (err) throw err;
-    else console.log("connected as id " + connection.threadId);
+    else console.log(`Success! Connected as id ${connection.threadId}.`);
 });
 
 // exporting the connection for sure in server.js
